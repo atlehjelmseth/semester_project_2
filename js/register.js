5550fdabf936ca264c66ba62592009073aece73f
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2024-01-27 17:24:11 +0100

    Register works

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 const api_base_url = 'https://api.noroff.dev';
 const register = document.getElementById("register");
 const registerUrl = `${api_base_url}/api/v1/auction/auth/register`;
-const wrong = document.querySelector(".wrongmailorpassword");
+const statusHtml = document.querySelector(".wrongmailorpassword");
 /**
  * Register the user to the API
  * @param {string} url 
@@ -11,7 +11,7 @@ const wrong = document.querySelector(".wrongmailorpassword");
  * ```
  */
 async function registerUser(url, userData) {
-  wrong.innerHTML = '';
+  statusHtml.innerHTML = '';
   try {
     const postData = {
       method: 'POST',
@@ -23,6 +23,23 @@ async function registerUser(url, userData) {
     const response = await fetch(url, postData);
     const json = await response.json();
     console.log(json);
+    if (response.status === 201) {
+      setTimeout(()=> {
+        location.href = "/login.html";
+     } ,3000);
+      console.log("Registration success");
+      statusHtml.innerHTML += `<p class="success">Registration success</p>`;
+    } else {
+      console.log("Could not registrate new user");
+    } 
+    if(json.statusCode === 400) {
+      for(let i = 0; i < json.errors.length; i++) {
+        const errorHtml = json.errors[i].message;
+        statusHtml.innerHTML += `<p class="error">${errorHtml}</p>`;
+      }
+    }else {
+      console.log("Registration was a success")
+    }
   } catch(error) {
     console.log(error);
   }
@@ -34,14 +51,12 @@ register.onclick = function (ev) {
   let name = document.getElementById("username").value;
   let email = document.getElementById("email").value.toLowerCase();
   let password = document.getElementById("password").value;
-  let picture = document.getElementById("avatar").value;
-  let avatar = picture.replace(/^.*\\/, "");
+
 
 const userToRegister = {
   name: name, 
   email: email,
   password: password,
-  avatar: avatar,
   }
   registerUser(registerUrl, userToRegister)
 }
